3cf1195ecaf58b849afc5c482c2f7258e9910d86
Gautam  Nag <51375618+gautamnag279@users.noreply.github.com>
2023-01-18 14:58:39 +0530

    updated with faster solution [108ms -> 92ms]

--- Codeforces/800/1703A - YES or YES?.py
+++ Codeforces/800/1703A - YES or YES?.py
@@ -10,3 +10,16 @@ if __name__ == "__main__":
     cases = int(input())
     for i in range(cases):
         codeforces(input())
+
+# 92 ms, 3700 KB
+def codeforces(s):
+    if s in ["yes", "YES", "Yes", "yEs", "yeS", "YEs", "yES", "YeS"]:
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == "__main__":
+    cases = int(input())
+    for i in range(cases):
+        codeforces(input())
