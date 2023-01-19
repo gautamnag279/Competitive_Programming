d5d6dd051a1a34f628902d5bbad7f9088e055866
Gautam  Nag <51375618+gautamnag279@users.noreply.github.com>
2023-01-19 18:23:13 +0530

    added faster solution [140ms -> 93ms]

--- Codeforces/800/1676A - Lucky?.py
+++ Codeforces/800/1676A - Lucky?.py
@@ -12,4 +12,25 @@ if __name__ == "__main__":
     items = int(input())
     for i in range(items):
         codeforces(int(input()))
+        
+# 93 ms, 3500 KB 
+def codeforces(s):
+    a = int(s[0])
+    b = int(s[1])
+    c = int(s[2])
+    
+    x = int(s[3])
+    y = int(s[4])
+    z = int(s[5])
+
+    if (a+b+c == x+y+z):
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == "__main__":
+    items = int(input())
+    for i in range(items):
+        codeforces(input())
 
